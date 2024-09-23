32d39bd118c8962b5a6834e15178a0d4a7e8952f
Friss0 <facundogberisso@gmail.com>
2024-09-23 01:59:11 -0300

    Modifico el archivo main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     # creamos membresias ficticias 
     membresia1 = Membresia(1, "Básico", 20.0, ["Acceso 1 sede", "Rutinas nivel 1", "Consultas via Mail"])
     membresia2 = Membresia(2, "Medio", 50.0, ["Acceso a todas las sedes", "Rutinas nivel 1 y 2", "Acceso a 2 clases especiales por mes"])
-    membresia3 = Membresia(3, "VIP", 100.0, ["Acceso a gimnasio", "Clases en grupo", "Entrenador personal", "Spa"])
+    membresia3 = Membresia(3, "VIP", 100.0, ["Acceso a todas las sedes", "Rutinas nivel 1, 2 y 3", "Consultas via mail y llamadas virtuales", "Acceso a clases especiales en cada  sede"])
 
     # mostramos las membresias que creamos 
     membresia1.mostrar_membresia()
