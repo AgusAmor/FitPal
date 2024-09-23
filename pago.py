709f3d17fa3698e73c044d0e1501a4e88f8fa051
Agustin Amor <agustinwamor@gmail.com>
2024-09-23 00:39:36 -0300

    Creacion de clases para FitPal

---
+++ pago.py
@@ -0,0 +1,5 @@
+class pago:
+    def __init__(self, id_pago, metodo_pago, monto):
+        self.id_pago = id_pago
+        self.metodo_pago = metodo_pago
+        self.monto = monto
---
+++ persona.py
@@ -0,0 +1,6 @@
+class persona:
+    def __init__(self, id_persona, nombre, apellido, dni):
+        self.id_persona = id_persona
+        self.nombre = nombre
+        self.apellido = apellido
+        self.dni = dni
