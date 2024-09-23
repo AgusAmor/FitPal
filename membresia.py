8ef5879918313ed94800cec05d3cf99a1872e183
marianbera <marianoberasaluce@gmail.com>
2024-09-23 00:48:22 -0300

    Nuevas Actualizaciones!

--- membresia.py
+++ membresia.py
@@ -1,5 +1,6 @@
 class membresia:
-    def __init__(self, id_membresia, nombre, precio):
+    def __init__(self, id_membresia, nombre, precio,beneficios):
         self.id_membresia = id_membresia
         self.nombre = nombre
         self.precio = precio
+        self.beneficios = beneficios
--- persona.py
+++ persona.py
@@ -1,6 +1,8 @@
 class persona:
-    def __init__(self, id_persona, nombre, apellido, dni):
+    def __init__(self, id_persona, nombre, apellido, dni, id_membresia, cliente_pago):
         self.id_persona = id_persona
         self.nombre = nombre
         self.apellido = apellido
+        self.id_membresia = id_membresia
         self.dni = dni
+        self.cliente_pago = cliente_pago
